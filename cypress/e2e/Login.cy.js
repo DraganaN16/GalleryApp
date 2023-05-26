3329aa6181abe9b2b568c8d83ccc0fe5e4f1738d
DraganaN16 <dragananikolic160685@gmail.com>
2023-05-26 18:35:24 +0200

    comiting

---
+++ cypress/e2e/Login.cy.js
@@ -0,0 +1,50 @@
+/// <reference types='cypress'/>
+
+describe("Login page", () => {
+  it("Log in with valid email and password- PO", () => {
+    cy.visit("login");
+    cy.get("#email").type("draganan@gmail.com");
+    cy.get("#password").type("pokusavam100");
+    cy.get("button[type='submit']").click();
+    cy.wait(2000);
+  });
+});
+
+describe("Login page", () => {
+  it("Log in without credentials- NEG", () => {
+    cy.visit("login");
+    cy.get("button[type='submit']").click();
+    cy.wait(2000);
+  });
+
+  it("Log in without email- NEG", () => {
+    cy.visit("login");
+    cy.get("#password").type("pokusavam100");
+    cy.get("button[type='submit']").click();
+    cy.wait(2000);
+  });
+
+  it("Log in without password- NEG", () => {
+    cy.visit("login");
+    cy.get("#email").type("draganan@gmail.com");
+    cy.get("button[type='submit']").click();
+    cy.wait(2000);
+  });
+
+  it("Log in with valid email and invalid password- NEG", () => {
+    cy.visit("login");
+    cy.get("#email").type("dragall@gmail.com");
+    cy.get("#password").type("pokusavam100");
+    cy.get("button[type='submit']").click();
+    cy.wait(2000);
+  });
+
+  it("Log in with invalid email and invalid password- NEG", () => {
+    cy.visit("login");
+    cy.get("#email").type("dragall@gmail.com");
+    cy.get("#password").type("test");
+    cy.get("button[type='submit']").click();
+    cy.wait(2000);
+  });
+
+});
