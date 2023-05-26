31aa7bf53e1db5ece824bfc13223b895a723a80a
DraganaN16 <dragananikolic160685@gmail.com>
2023-05-26 22:29:00 +0200

    commiting

--- cypress/e2e/Login.cy.js
+++ cypress/e2e/Login.cy.js
@@ -1,49 +1,71 @@
 /// <reference types='cypress'/>
 
-describe("Login page", () => {
-  it("Log in with valid email and password- PO", () => {
-    cy.visit("login");
-    cy.get("#email").type("draganan@gmail.com");
-    cy.get("#password").type("pokusavam100");
-    cy.get("button[type='submit']").click();
+const locators = require("../fixtures/locators.json");
+
+import {faker} from '@faker-js/faker'
+
+var email
+var password
+
+
+before(() => {
+  cy.clearAllCookies()
+  cy.clearAllLocalStorage()
+  cy.clearAllSessionStorage()
+})
+
+
+beforeEach(() => {
+  cy.visit("login");
+}) 
+
+describe("Login page- PO", () => {
+  it("Log in with valid email and password", () => {
+
+    email = faker.internet.email();
+    password = faker.internet.password();
+
+    cy.get(locators.loginPage.emailInputField).type(email);
+    cy.get(locators.loginPage.passwordInputField).type(password);
+    cy.get(locators.loginPage.submitButton).click();
     cy.wait(3000);
   });
 });
 
-describe("Login page", () => {
-  it("Log in without credentials- NEG", () => {
-    cy.visit("login");
-    cy.get("button[type='submit']").click();
+
+describe("Login page- NEG", () => {
+
+  email = faker.internet.email();
+  password = faker.internet.password(); 
+
+  it("Log in without credentials", () => {
+    cy.get(locators.loginPage.submitButton).click();
     cy.wait(3000);
   });
 
-  it("Log in without email- NEG", () => {
-    cy.visit("login");
-    cy.get("#password").type("pokusavam100");
-    cy.get("button[type='submit']").click();
+  it("Log in without email", () => {
+    cy.get(locators.loginPage.passwordInputField).type(password);
+    cy.get(locators.loginPage.submitButton).click();
     cy.wait(3000);
   });
 
-  it("Log in without password- NEG", () => {
-    cy.visit("login");
-    cy.get("#email").type("draganan@gmail.com");
-    cy.get("button[type='submit']").click();
+  it("Log in without password", () => {
+    cy.get(locators.loginPage.emailInputField).type(email);
+    cy.get(locators.loginPage.submitButton).click();
     cy.wait(3000);
   });
 
-  it("Log in with valid email and invalid password- NEG", () => {
-    cy.visit("login");
-    cy.get("#email").type("dragall@gmail.com");
-    cy.get("#password").type("pokusavam100");
-    cy.get("button[type='submit']").click();
+  it("Log in with valid email and invalid password", () => {
+    cy.get(locators.loginPage.emailInputField).type(email);
+    cy.get(locators.loginPage.passwordInputField).type(password);
+    cy.get(locators.loginPage.submitButton).click();
     cy.wait(3000);
   });
 
-  it("Log in with invalid email and invalid password- NEG", () => {
-    cy.visit("login");
-    cy.get("#email").type("dragall@gmail.com");
-    cy.get("#password").type("test");
-    cy.get("button[type='submit']").click();
+  it("Log in with invalid email and invalid password", () => {
+    cy.get(locators.loginPage.emailInputField).type(email);
+    cy.get(locators.loginPage.passwordInputField).type(password);
+    cy.get(locators.loginPage.submitButton).click();
     cy.wait(3000);
   });
 
